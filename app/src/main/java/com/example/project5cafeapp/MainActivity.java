83eddd6d92730e4f0e7a7d482397789757108a31
HyeonOh678 <bryanhasu@gmail.com>
2023-04-26 19:29:45 -0400

    comments to main activity class

--- app/src/main/java/com/example/project5cafeapp/MainActivity.java
+++ app/src/main/java/com/example/project5cafeapp/MainActivity.java
@@ -1,3 +1,6 @@
+/**
+ * Project package
+ */
 package com.example.project5cafeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,11 @@ import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
+/**
+ * This class is the main activity/main controller of the mobile application
+ * This is the first activity page that the user will see upon entering the application
+ * Defines the image buttons used to redirect the user to other views
+ */
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton mainDonutButton;
@@ -28,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton orderBasketButton;
     private ImageButton storeOrdersButton;
 
+    /**
+     * Sets up the view for the "front page" of what the user sees when opening the application
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SharedPreferences pref = getSharedPreferences("data_shared",MODE_PRIVATE);
@@ -47,6 +62,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mainDonutButton = findViewById(R.id.mainDonut);
         mainDonutButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * When clicking on the donuts image button, the user will be redirected to the donut ordering page
+             * @param v The view that was clicked.
+             */
             @Override
             public void onClick(View v) {
                 Intent intentLoadNewActivity = new Intent(MainActivity.this, donutActivity.class);
@@ -55,6 +74,10 @@ public class MainActivity extends AppCompatActivity {
         });
         mainCoffeeButton = findViewById(R.id.mainCoffee);
         mainCoffeeButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * When clicking on the coffee image button, the user will be redirected to the coffee ordering page
+             * @param v The view that was clicked.
+             */
             @Override
             public void onClick(View v) {
                 Intent intentLoadNewActivity = new Intent(MainActivity.this, coffeeActivity.class);
@@ -63,6 +86,10 @@ public class MainActivity extends AppCompatActivity {
         });
         orderBasketButton = findViewById(R.id.orderBasket);
         orderBasketButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * When clicking on the basket image button, the user will be redirected to the basket page
+             * @param v The view that was clicked.
+             */
             @Override
             public void onClick(View v) {
                 Intent intentLoadNewActivity = new Intent(MainActivity.this, basketActivity.class);
@@ -71,6 +98,10 @@ public class MainActivity extends AppCompatActivity {
         });
         storeOrdersButton = findViewById(R.id.storeOrders);
         storeOrdersButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * When clicking on the store image button, the user will be redirected to the store page
+             * @param v The view that was clicked.
+             */
             @Override
             public void onClick(View v) {
                 Intent intentLoadNewActivity = new Intent(MainActivity.this, storeActivity.class);
