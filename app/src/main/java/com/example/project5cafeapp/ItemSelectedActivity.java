068a796944fb893b6c022c32418f457362e12822
HyeonOh678 <bryanhasu@gmail.com>
2023-04-26 19:44:02 -0400

    comments to itemselectedactivity class

--- app/src/main/java/com/example/project5cafeapp/ItemSelectedActivity.java
+++ app/src/main/java/com/example/project5cafeapp/ItemSelectedActivity.java
@@ -1,3 +1,6 @@
+/**
+ * Project package
+ */
 package com.example.project5cafeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -36,6 +39,10 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
+/**
+ * When an item displayed in a row on the RecyclerView is clicked, the user will be redirected to a new activity
+ * In this activity, the user will be able to select the amount of donuts from the donut flavor picked and add the donut order to the basket
+ */
 public class ItemSelectedActivity extends AppCompatActivity {
     private Button btn_itemName;
     private ImageView donutImageView;
@@ -46,6 +53,13 @@ public class ItemSelectedActivity extends AppCompatActivity {
     private String [] donutAmounts = {"1", "2", "3", "4", "5","6"};
     private ArrayAdapter<String> adapter;
 
+    /**
+     * Sets up the view and adapters
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,6 +77,13 @@ public class ItemSelectedActivity extends AppCompatActivity {
         Intent intent = getIntent();
         donutFlavor.setText(intent.getStringExtra("ITEM"));
         donutAmountSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            /**
+             * When selecting an item, determines the donut flavor and type and assigns prices accordingly
+             * @param parent The AdapterView where the selection happened
+             * @param view The view within the AdapterView that was clicked
+             * @param position The position of the view in the adapter
+             * @param id The row id of the item that is selected
+             */
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 price = findViewById(R.id.price);
@@ -86,6 +107,10 @@ public class ItemSelectedActivity extends AppCompatActivity {
                 }
             }
 
+            /**
+             * if nothing is selected, the price is set at $0.00
+             * @param parent The AdapterView that now contains no selected item.
+             */
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 price.setText("$0.00");
@@ -93,16 +118,38 @@ public class ItemSelectedActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * When clicking on the "Add to Basket" button, the user will be met with an alert dialog
+     * The alert dialog will notify the users whether they want to add the donut order to the basket
+     * @param view
+     */
     public void addDonutToBasket(View view){
         AlertDialog.Builder alert = new AlertDialog.Builder(view.getContext());
         alert.setTitle("Add to Order");
         alert.setMessage(donutAmountSpinner.getSelectedItem().toString() +" x "+ donutFlavorSelected.getText()+"\n"+price.getText().toString().split(" ")[1]);
         alert.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            /**
+             * When adding the donut, an AlertDialog will be shown to the user which will give the user a choice of adding the order to the basket
+             * When clicking yes, the order will be added
+             * Will tell the user with a fading message the donut amount, type, and flavor added to the basket
+             * @param dialog the dialog that received the click
+             * @param which the button that was clicked (ex.
+             *              {@link DialogInterface#BUTTON_POSITIVE}) or the position
+             *              of the item clicked
+             */
             public void onClick(DialogInterface dialog, int which) {
                 addToBasket(view);
             }
             //handle the "NO" click
         }).setNegativeButton("no", new DialogInterface.OnClickListener() {
+            /**
+             * When adding the donut, an AlertDialog will be shown to the user which will give the user a choice of adding the order to the basket
+             * When clicking no, the order will not be added and will display "Item Not Added."
+             * @param dialog the dialog that received the click
+             * @param which the button that was clicked (ex.
+             *              {@link DialogInterface#BUTTON_POSITIVE}) or the position
+             *              of the item clicked
+             */
             public void onClick(DialogInterface dialog, int which) {
                 Toast.makeText(view.getContext(),
                         "Item Not Added.", Toast.LENGTH_LONG).show();
@@ -112,6 +159,10 @@ public class ItemSelectedActivity extends AppCompatActivity {
         dialog.show();
     }
 
+    /**
+     * adds the donut order to the basket
+     * @param view
+     */
     private void addToBasket(View view){
         MenuItem menuItem = getMenuItem(donutFlavorSelected.getText().toString());
         String amount = donutAmountSpinner.getSelectedItem().toString();
@@ -143,10 +194,20 @@ public class ItemSelectedActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * formats the price
+     * @param ourDouble
+     * @return
+     */
     private String formatDouble(double ourDouble){
         return format(Math.round(ourDouble*100.00) / 100.00);
     }
 
+    /**
+     * Fetches the MenuItem which is the donut flavor type
+     * @param name
+     * @return
+     */
     private MenuItem getMenuItem(String name){
         String[] nameArray = name.split(" ");
         if(nameArray.length>1){
@@ -201,6 +262,11 @@ public class ItemSelectedActivity extends AppCompatActivity {
         return null;
     }
 
+    /**
+     * formatting for the price
+     * @param num
+     * @return
+     */
     private String format(double num){
         String numString = Double.toString(num);
         String back = numString.split("\\.")[1];
