4259ad63b6468311c0f57412b2e3c730e1d5a464
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-04-26 14:57:07 -0400

    changes

--- app/src/main/java/com/example/project5cafeapp/coffeeActivity.java
+++ app/src/main/java/com/example/project5cafeapp/coffeeActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,7 +22,12 @@ public class coffeeActivity extends AppCompatActivity{
     private String [] coffeeAmount = {"1", "2", "3", "4", "5"};
     private ArrayAdapter<String> adapter1;
     private ArrayAdapter<String> adapter2;
-
+    private TextView total;
+    private CheckBox sweetCreamBox;
+    private CheckBox mochaBox;
+    private CheckBox vanillaBox;
+    private CheckBox caramelBox;
+    private CheckBox creamBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +37,21 @@ public class coffeeActivity extends AppCompatActivity{
         coffeeSizeSpinner = findViewById(R.id.coffeeSize);
         adapter1 = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, coffeeSizes);
         coffeeSizeSpinner.setAdapter(adapter1);
-        //setonitemselectedlistener here sometime
 
         coffeeAmountSpinner = findViewById(R.id.coffeeAmount);
         adapter2 = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, coffeeAmount);
         coffeeAmountSpinner.setAdapter(adapter2);
 
+        total = findViewById(R.id.editTextText);
+        total.setText("Total: $1.89");
+    }
 
+    public void updatePrice(View view){
+        boolean sc = sweetCreamBox.isChecked();
+        boolean m = mochaBox.isChecked();
+        boolean fv = vanillaBox.isChecked();
+        boolean c = caramelBox.isChecked();
+        boolean ic = creamBox.isChecked();
+        
     }
 }
